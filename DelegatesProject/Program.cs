f5ba3d7df54f500e78de09a0b8eb4b265531360d
waqar <wakku9780@gmail.com>
2024-02-24 00:57:37 +0530

    Add project files.

---
+++ DelegatesProject/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace DelegatesProject
+{
+    //Defining a Delegeta 
+    public delegate void AddDelegate(int x, int y);
+    public delegate string  SayDelegate(string str);
+    public  class Program
+    {
+        public void AddNums(int a,int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static string SayHello(string name)
+        {
+            return "Hello " + name;
+        }
+       static void Main(string[] args)
+        {
+            Program p = new Program();
+            // p.AddNums(100, 50);
+
+            //string str = Program.SayHello("waqar");
+            //Console.WriteLine(str);
+            //Console.ReadLine();
+
+
+            AddDelegate ad = new AddDelegate(p.AddNums);
+            ad(100, 50);
+            SayDelegate sd = new SayDelegate(Program.SayHello);
+            string str = sd("Raju"); 
+
+            string str1 = Program.SayHello("Raju");
+
+            Console.WriteLine(str1);
+
+            Console.ReadLine(); 
+       }
+    }
+}
---
+++ DelegatesProject/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//MultiCast Delegate
+namespace DelegatesProject
+{
+    public delegate void RectDelegate(double Width, double Height);
+    internal class Rectangle
+    {
+        public void GetArea(double Width,double Height)
+        {
+            Console.WriteLine("Area of Rectangle"+Width * Height);
+        }
+        public void GetPerimeter(double Width,double Height)
+        {
+            Console.WriteLine("Perimeter of Rectangle"+2*(Width + Height));
+
+        }
+        static void Main()
+        {
+            Rectangle rect = new Rectangle();
+            RectDelegate obj = rect.GetArea;
+            obj += rect.GetPerimeter; 
+            obj.Invoke(12.34, 56.78);
+            Console.WriteLine();
+            obj.Invoke(47.87, 34.89);
+            Console.ReadLine(); 
+        }
+    }
+}
